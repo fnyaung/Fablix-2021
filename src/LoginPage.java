ab00f8c9fa22b0c50e5a98d34c52a8b8274b2fdb
Alice Kim <hyejik5@uci.edu>
2021-05-09 18:47:03 -0700

    login_made

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,5 +1,5 @@
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
@@ -9,10 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginPage extends HttpServlet {
@@ -37,45 +39,120 @@ public class LoginPage extends HttpServlet {
         // Retrieve parameter id from url request.
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String location = request.getParameter("location");
 
-        System.out.println(" " + username + " " + password);
+        // get recaptcha
+        String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
 
-        JsonObject responseJsonObject = new JsonObject();
-
+        // Verify reCAPTCHA
         try {
-            Connection dbcon = dataSource.getConnection();
-            String query = "SELECT * FROM customers where email = ? and password = ?";
-            // Declare our statement
-            PreparedStatement statement = dbcon.prepareStatement(query);
-            // Replace ? with username
-            statement.setString(1,username);
-            statement.setString(2,password);
+            System.out.println("!!!");
+            RecaptchaVerifyUtils.verify(gRecaptchaResponse);
+            System.out.println("!!!");
 
-            ResultSet rs = statement.executeQuery();
 
-            if(rs.next()) { // there is a result
-                // set this user into the session
-                User user = new User(username);
-                request.getSession().setAttribute("user", user);
-                // set user with its corresponding ID
-                user.setUserID(rs.getInt("id"));
-                responseJsonObject.addProperty("status", "success");
-                responseJsonObject.addProperty("message", "success");
+            System.out.println("username: " + username);
+            System.out.println("password: " + password);
+            System.out.println("location: " + location);
 
-            } else { // there is no result
-                // Login fail
-                responseJsonObject.addProperty("status", "fail");
 
-                // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-                if (!username.equals("anteater")) {
-                    responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
-                } else {
-                    responseJsonObject.addProperty("message", "incorrect password");
+            JsonObject responseJsonObject = new JsonObject();
+
+            try {
+                String query;
+                Connection dbcon = dataSource.getConnection();
+//            query = "SELECT * FROM customers where email = ? ";
+
+                if(location.equals("customer")){
+                    // user
+                    System.out.println("Customer");
+                    query = "SELECT * FROM customers where email = ? ";
                 }
+                else{
+                    // staff
+                    System.out.println("Employee");
+                    query = "SELECT * FROM employees where email = ? ";
+                }
+
+                // Declare our statement
+                PreparedStatement statement = dbcon.prepareStatement(query);
+                // Replace ? with username
+                statement.setString(1,username);
+
+                ResultSet rs = statement.executeQuery();
+
+                System.out.println("statement");
+                System.out.println(statement);
+//            /*
+                if(rs.next()) { // there is a result
+                    System.out.println("rs.next()");
+
+                    String encryptedPassword = rs.getString("password");
+                    boolean success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
+
+                    System.out.println("Password success? : " + success);
+
+                    if (success){
+                        System.out.println("Inside the if statement : getting user into the session");
+
+                        // set this user into the session
+                        User user = new User(username);
+                        request.getSession().setAttribute("user", user);
+
+                        if(location.equals("customer")){
+                            // set user with its corresponding ID
+                            System.out.println(rs.getInt("id"));
+                            user.setUserID(rs.getInt("id"));
+                        }
+
+                        // set the user with its user type
+//                    user.setUserType(location);
+
+                        responseJsonObject.addProperty("status", "success");
+                        responseJsonObject.addProperty("usertype", location);
+                        responseJsonObject.addProperty("message", "success");
+
+                        System.out.println(responseJsonObject);
+                    }
+                    else{
+                        System.out.println("Inside the else statement : getting user into the session");
+                        responseJsonObject.addProperty("status", "fail");
+                        if(!success) {
+                            responseJsonObject.addProperty("message", "incorrect password");
+                        }
+                    }
+
+
+                } else { // there is no result
+                    // Login fail
+                    responseJsonObject.addProperty("status", "fail");
+
+                    // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
+
+                    if (!rs.next()) {
+                        responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+                    }
+
+                }
+                response.getWriter().write(responseJsonObject.toString());
+                response.setStatus(200);
+
+                // Closing after opening
+                rs.close();
+                statement.close();
+                dbcon.close();
+
+            } catch (Exception e) {
+                // write error message JSON object to output
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("errorMessage", e.getMessage());
+
+                // set reponse status to 500 (Internal Server Error)
+                response.setStatus(500);
             }
-            response.getWriter().write(responseJsonObject.toString());
 
         } catch (Exception e) {
+            System.out.println("Error");
             // write error message JSON object to output
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("errorMessage", e.getMessage());
@@ -83,6 +160,7 @@ public class LoginPage extends HttpServlet {
             // set reponse status to 500 (Internal Server Error)
             response.setStatus(500);
         }
+        // Verify reCAPTCHA
 
     }
 }
